abce96e0b43113d2b66ab3ab4bc4a166a1a693ce
Samuel-Weir <s.weir@ngenroom.co.nz>
2025-04-28 14:01:14 +1200

    part of module 5

---
+++ Module 5 - Conditionals/projects/Project-1/adventureGame.js
@@ -0,0 +1,15 @@
+let direction = "left"
+let hasKey = true
+function waytogo(dir){
+    if (dir == "forward")
+        return "this is the way to go"
+    else if (dir == "left")
+        return "You are dumb stupid idiot"
+    else if (dir == "right")
+        return "You be so lost"
+    else return "please pick a direction"
+}
+function lockedDoor(){
+    if 
+}
+console.log(waytogo(direction))
